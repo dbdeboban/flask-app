a9f50c95903f99b4844ea923c612704d9a18c835
dbdeboban <dbdeboban@gmail.com>
2019-07-17 09:54:42 +0530

    latest commit

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Api
-from app import flaskAppInstance
-from .UserApi import User
-
-
-
-restServerInstance = Api(flaskAppInstance)
-
-restServerInstance.add_resource(User,"/api/User/Signup")
